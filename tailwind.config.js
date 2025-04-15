92925e352522edca10dd489697b199c279439c9c
Wilfredo Caba Ramos <wcabaramos@gmail.com>
2025-04-15 09:40:08 -0400

    Tailwind interno

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./javascript/**/*.js",
+    "./style/**/*.css",
+    "./utility/**/*.{html,js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
